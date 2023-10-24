93d57b505160707122b44cf9270954dec2ce9461
ZubriQ <zubrich157@gmail.com>
2023-10-25 02:23:50 +0700

    feat: AuthorizationService.cs for log-in page

--- Travel-Company.WPF/App.xaml.cs
+++ Travel-Company.WPF/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using Travel_Company.WPF.Core;
 using Travel_Company.WPF.MVVM.View;
 using Travel_Company.WPF.MVVM.ViewModel;
+using Travel_Company.WPF.Services.Authorization;
 using Travel_Company.WPF.Services.Navigation;
 
 namespace Travel_Company.WPF
@@ -24,12 +25,23 @@ namespace Travel_Company.WPF
                 DataContext = provider.GetRequiredService<MainViewModel>()
             });
             services.AddSingleton<INavigationService, NavigationService>();
-            services.AddSingleton<MainViewModel>();
-            services.AddSingleton<LoginViewModel>();
             services.AddSingleton<Func<Type, ViewModel>>(
                 serviceProvider => viewModelType => (ViewModel)serviceProvider.GetRequiredService(viewModelType));
+            InitializeViewModels(services);
+            InitializeDbServices(services);
 
             _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private static void InitializeViewModels(IServiceCollection services)
+        {
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<LoginViewModel>();
+        }
+        
+        private static void InitializeDbServices(IServiceCollection services)
+        {
+            services.AddScoped<IAuthorizationService, AuthorizationService>();
         }
 
         protected override void OnStartup(StartupEventArgs e)
