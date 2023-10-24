8194d20efd092a1c97a666cdecbd61fc8874f2f8
ZubriQ <zubrich157@gmail.com>
2023-10-25 02:23:50 +0700

    feat: AuthorizationService.cs for log-in page

---
+++ Travel-Company.WPF/Services/Authorization/AuthorizationService.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using Travel_Company.WPF.Models;
+using Travel_Company.WPF.Models.Users;
+
+namespace Travel_Company.WPF.Services.Authorization;
+
+public class AuthorizationService : IAuthorizationService
+{
+    private readonly TravelCompanyDbContext _context;
+
+    public AuthorizationService(TravelCompanyDbContext context)
+    {
+        _context = context;
+    }
+    
+    public User? LogIn(string username, string password)
+    {
+        if (_context.Users.FirstOrDefault(u => u.Username == username) is { } user && user.Password == password)
+        {
+            return user;
+        }
+
+        return null!;
+    }
+}
