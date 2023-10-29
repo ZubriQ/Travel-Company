6a819f53f3906df19a867f38215d744254315f4d
ZubriQ <zubrich157@gmail.com>
2023-10-29 16:38:06 +0700

    feat: hotels and populated places seeding

--- Travel-Company.WPF/Data/DbInitializer.cs
+++ Travel-Company.WPF/Data/DbInitializer.cs
@@ -20,6 +20,8 @@ public static class DbInitializer
     private static void SeedAllEntities(TravelCompanyDbContext db)
     {
         SeedStreets(db);
+        SeedHotels(db);
+        SeedPopulatedPlaces(db);
     }
 
     private static void SeedStreets(TravelCompanyDbContext db)
@@ -30,19 +32,69 @@ public static class DbInitializer
         }
 
         var streets = new List<Street>
-            {
-                new Street() { Name = "Abbey Road" },
-                new Street() { Name = "Baker Street" },
-                new Street() { Name = "Buckingham Palace Road" },
-                new Street() { Name = "Carlisle Street" },
-                new Street() { Name = "Cavendish Square" },
-                new Street() { Name = "Downing Street" },
-                new Street() { Name = "Eaton Square" },
-                new Street() { Name = "Fitzroy Street" },
-                new Street() { Name = "Great George Street" },
-                new Street() { Name = "Hanover Square" },
-            };
+        {
+            new Street() { Name = "Abbey Road" },
+            new Street() { Name = "Baker Street" },
+            new Street() { Name = "Buckingham Palace Road" },
+            new Street() { Name = "Carlisle Street" },
+            new Street() { Name = "Cavendish Square" },
+            new Street() { Name = "Downing Street" },
+            new Street() { Name = "Eaton Square" },
+            new Street() { Name = "Fitzroy Street" },
+            new Street() { Name = "Great George Street" },
+            new Street() { Name = "Hanover Square" },
+        };
         db.AddRange(streets);
+        db.SaveChanges();
+    }
+
+    private static void SeedHotels(TravelCompanyDbContext db)
+    {
+        if (db.Hotels.Any())
+        {
+            return;
+        }
+
+        var hotels = new List<Hotel>
+        {
+            new Hotel() { Name = "The Royal Crown Hotel" },
+            new Hotel() { Name = "Windsor Manor" },
+            new Hotel() { Name = "Thamesview Hotel & Spa" },
+            new Hotel() { Name = "Highland Retreat" },
+            new Hotel() { Name = "Coastal Haven Inn" },
+            new Hotel() { Name = "Victoria Grand Hotel" },
+            new Hotel() { Name = "Greenwich Park Lodge" },
+            new Hotel() { Name = "The Lakeside Retreat" },
+            new Hotel() { Name = "Cambridge Riverside Inn" },
+            new Hotel() { Name = "Edinburgh Castle Hotel" },
+        };
+
+        db.AddRange(hotels);
+        db.SaveChanges();
+    }
+
+    private static void SeedPopulatedPlaces(TravelCompanyDbContext db)
+    {
+        if (db.Hotels.Any())
+        {
+            return;
+        }
+
+        var populatedPlaces = new List<PopulatedPlace>
+        {
+            new PopulatedPlace() { Name = "London" },
+            new PopulatedPlace() { Name = "Manchester" },
+            new PopulatedPlace() { Name = "Birmingham" },
+            new PopulatedPlace() { Name = "Liverpool" },
+            new PopulatedPlace() { Name = "Glasgow" },
+            new PopulatedPlace() { Name = "Leeds" },
+            new PopulatedPlace() { Name = "Bristol" },
+            new PopulatedPlace() { Name = "Sheffield" },
+            new PopulatedPlace() { Name = "Edinburgh" },
+            new PopulatedPlace() { Name = "Cardiff" },
+        };
+
+        db.AddRange(populatedPlaces);
         db.SaveChanges();
     }
 }
